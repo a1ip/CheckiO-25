8c6cf3b86e684f5616cf8e472de46ca8d2c33c67
Iurii Zdebskyi <yuriy.zdebskyi@gmail.com>
2015-01-29 17:13:37 -0800

    Secret Message
    You are given a chunk of text. Gather all capital letters in one word in
    the order that they appear in the text.

---
+++ Secret Message/Secret Message.py
@@ -0,0 +1,15 @@
+def find_message(text):
+
+    word = ""
+    for letter in text:
+        if letter.isupper():
+            word += letter
+
+
+    return word
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert find_message("How are you? Eh, ok. Low or Lower? Ohhh.") == "HELLO", "hello"
+    assert find_message("hello world!") == "", "Nothing"
+    assert find_message("HELLO WORLD!!!") == "HELLOWORLD", "Capitals"
