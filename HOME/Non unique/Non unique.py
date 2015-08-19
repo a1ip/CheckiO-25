8720d8c6914e33b00065fc777d2e73ba7ba5e749
zdebyman <yuriy.zdebskyi@gmail.com>
2015-08-18 22:23:16 -0700

    Non unique elements + refactoring
    Input: A list of integers.
    Output: The list of integers.
    
    Example:
    checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3]
    checkio([1, 2, 3, 4, 5]) == []
    checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
    checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]

---
+++ HOME/Non unique/Non unique.py
@@ -0,0 +1,17 @@
+def checkio(data):
+    result = []
+
+    while len(data) > 0:
+        current = data.pop()
+        if current in data or current in result:
+           result.insert(0, current)
+    return result
+
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(checkio([1]), list), "The result must be a list"
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
