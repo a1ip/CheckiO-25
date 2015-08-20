280e0f453f9e48a91aabfce5c882636d8510e717
zdebyman <yuriy.zdebskyi@gmail.com>
2015-08-20 00:43:42 -0700

    Moore Neighbourhood
    Input: Three arguments. A grid as a tuple of tuples with integers (1/0),
    a row number and column number for a cell as integers.
    
    Output: How many neighbouring cells have chips as an integer.

---
+++ HOME/Moore Neighbourhood/Moore Neighbourhood.py
@@ -0,0 +1,30 @@
+def count_neighbours(grid, row, col):
+    result = 0
+
+    for r in range(-1, 2):
+        for c in range(-1, 2):
+            if (0 <= row + r <= len(grid) - 1 and 0 <= col + c <= len(grid[0]) - 1):
+                if (grid[row + r][col + c] == 1) and not (r == 0 and c == 0):
+                    result += 1
+
+    return result
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 1, 2) == 3, "1st example"
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example"
+    assert count_neighbours(((1, 1, 1),
+                             (1, 1, 1),
+                             (1, 1, 1),), 0, 2) == 3, "Dense corner"
+    assert count_neighbours(((0, 0, 0),
+                             (0, 1, 0),
+                             (0, 0, 0),), 1, 1) == 0, "Single"
